4c6d8e936897d41d48f9f31cf0bed5307affea08
chadders <matthew-github@chadders.org>
2025-03-30 19:31:14 +1100

    Add feature to free memory from a TW_Timer object

--- src/engine/timer.h
+++ src/engine/timer.h
@@ -7,9 +7,9 @@
 
 // TW_Timer object
 typedef struct TW_Timer {
-    Uint64 currentTime;     // Time the clock was created in milliseconds.
-    bool paused;            // Is the timer paused - true = yes, false = no. Default = false
-    Uint64 pauseTime;       // Time the clock was paused in milliseconds.
+    Uint64 currentTime;     // Time the clock was created in milliseconds
+    bool paused;            // Is the timer paused? true = yes, false = no. Default = false
+    Uint64 pauseTime;       // Time the clock was paused in milliseconds
     Uint64 pauseDuration;   // How long has the timer been paused for? Default = 0
 } TW_Timer;
 
@@ -20,3 +20,4 @@ void TW_Timer_Reset( TW_Timer* self );
 void TW_Timer_Pause( TW_Timer* self );
 void TW_Timer_Resume( TW_Timer* self );
 Uint64 TW_Timer_GetTime( TW_Timer* self );
+void TW_Timer_Free( TW_Timer* self );
